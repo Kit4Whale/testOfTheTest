8b3e166a618972a0bf0ed9d19ae1b7b31ea901d2
Kit4Whale <60572662+Kit4Whale@users.noreply.github.com>
2022-09-09 00:09:39 +0300

    Добавлено несколько функций

--- firstFinder/src/main/java/ru/stqa/pft/firstfinder/TestProgram.java
+++ firstFinder/src/main/java/ru/stqa/pft/firstfinder/TestProgram.java
@@ -1,8 +1,20 @@
 package ru.stqa.pft.firstfinder;
 
+import java.util.Scanner;
+
 public class TestProgram {
 
        public static void main (String[] args) {
-              System.out.println("Hello, Nikita!");
+              hello(getName());
+       }
+
+       public static void hello(String somebody) {
+              System.out.println("Hello " + somebody + "!");
+       }
+       public static String getName () {
+              Scanner input = new Scanner(System.in);
+              String name = input.next();
+
+              return name;
        }
 }
