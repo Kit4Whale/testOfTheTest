f568f7ab6e17253b18ff7735624fa97927343025
Kit4Whale <60572662+Kit4Whale@users.noreply.github.com>
2022-10-02 17:28:19 +0300

    Добавление классов Квадрат и Четырёх угольник

--- firstFinder/src/main/java/ru/stqa/pft/firstfinder/TestProgram.java
+++ firstFinder/src/main/java/ru/stqa/pft/firstfinder/TestProgram.java
@@ -6,12 +6,20 @@ public class TestProgram {
 
        public static void main (String[] args) {
               hello(getName());
+
+              Square s = new Square(2);
+              System.out.println("Площадь квадрата со сторонами " + s.l + " = " + s.area());
+
+              Rectangle r = new Rectangle(2, 4);
+              System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
+
        }
 
        public static void hello(String somebody) {
               System.out.println("Hello " + somebody + "!");
        }
        public static String getName () {
+              System.out.print("Введите Ваше имя: ");
               Scanner input = new Scanner(System.in);
               String name = input.next();
 
