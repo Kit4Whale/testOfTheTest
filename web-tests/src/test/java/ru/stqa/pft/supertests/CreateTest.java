48286cda64f458c25a67f10031883d2d884d29b4
Kit4Whale <60572662+Kit4Whale@users.noreply.github.com>
2022-10-11 01:02:53 +0300

    Оптимизация и улучшение читаемости кода теста

--- web-tests/src/test/java/ru/stqa/pft/supertests/CreateTest.java
+++ web-tests/src/test/java/ru/stqa/pft/supertests/CreateTest.java
@@ -13,16 +13,9 @@ public class CreateTest {
   private WebDriver driver;
 
   @BeforeMethod
-  public void setUp() {
+  public void setUp() throws InterruptedException {
     System.setProperty("webdriver.chrome.driver", "/Users/nikitastrahov/Documents/GitHub/testOfTheTest/web-tests/src/test/resources/chromedriver");
     driver = new ChromeDriver();
-  }
-  @AfterMethod
-  public void tearDown() {
-    driver.quit();
-  }
-  @Test
-  public void authorization() throws InterruptedException {
     driver.get("https://stepik.org/catalog");
     driver.manage().window().setSize(new Dimension(1440, 817));
     driver.findElement(By.id("ember239")).click();
@@ -30,6 +23,15 @@ public class CreateTest {
     driver.findElement(By.id("id_login_password")).sendKeys("cjhjrfctvbvbkkbvtnhjdsq");
     driver.findElement(By.xpath("//form[@id = 'login_form']/button")).click();
     Thread.sleep(1000);
+  }
+
+  @Test
+  public void authorization() {
     driver.findElement(By.id("ember13")).click();
+  }
+
+  @AfterMethod
+  public void tearDown() {
+    driver.quit();
   }
 }
