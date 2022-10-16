9eda580730263cbd961ad33a2843c680b9904d45
Kit4Whale <60572662+Kit4Whale@users.noreply.github.com>
2022-10-16 21:57:57 +0300

    Создание нового теста и Оптимизация кода

--- web-tests/src/test/java/ru/stqa/pft/supertests/CreateTest.java
+++ web-tests/src/test/java/ru/stqa/pft/supertests/CreateTest.java
@@ -1,37 +1,13 @@
 package ru.stqa.pft.supertests;
 
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.Dimension;
 
-public class CreateTest {
-
-  private WebDriver driver;
-
-  @BeforeMethod
-  public void setUp() throws InterruptedException {
-    System.setProperty("webdriver.chrome.driver", "/Users/nikitastrahov/Documents/GitHub/testOfTheTest/web-tests/src/test/resources/chromedriver");
-    driver = new ChromeDriver();
-    driver.get("https://stepik.org/catalog");
-    driver.manage().window().setSize(new Dimension(1440, 817));
-    driver.findElement(By.id("ember239")).click();
-    driver.findElement(By.id("id_login_email")).sendKeys("nikitastrakhov1@gmail.com");
-    driver.findElement(By.id("id_login_password")).sendKeys("cjhjrfctvbvbkkbvtnhjdsq");
-    driver.findElement(By.xpath("//form[@id = 'login_form']/button")).click();
-    Thread.sleep(1000);
-  }
+public class CreateTest extends TestBase {
 
   @Test
-  public void authorization() {
-    driver.findElement(By.id("ember13")).click();
+  public void testAuthorization() {
+      myEducationPage();
+      
   }
 
-  @AfterMethod
-  public void tearDown() {
-    driver.quit();
-  }
 }
